ff949dc7455e0dc60bdd7f3dfe22c420bc0b6682
bullandbear traderz <bullandbeartraderz@gmail.com>
2025-06-28 20:04:50 +0530

    first commit

--- day01/hello.sh
+++ day01/hello.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# This is script for TWS
+# points to remember when you try to create shell script
+# 1. which shell you are using? shebang or ksh or csh or fish
+# 2. #!/bin/bash is a shebang value
+# 3. interpreter will understand the language and read & reply
 
-echo "TWS: Hello Dosto"
+echo "mukesh: Hello Dosto"
 
-echo "Learners: DevOps Wale bhaiya , hum toh comment karenge"
 
-echo "TWS: Toh dosto, like bhi kar do"
+
+
+echo "Learners: 'trader Wale bhaiya' , hum toh comment karenge"
+
+
+
+
+echo "mukesh: Toh dosto, like bhi kar do"
